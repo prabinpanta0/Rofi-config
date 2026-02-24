a63a3605d4946e4440086d1c645351cd7529b195
prabinpanta0 <pantaprabin30@gmail.com>
2026-02-24 10:24:28 +0545

    Update Rofi configuration and spotlight script for improved functionality

--- spotlight.sh
+++ spotlight.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+if pgrep -x "rofi" > /dev/null; then
+    pkill -x "rofi"
+    exit 0
+fi
+
 rofi -show combi -sep $'\t' -display-columns 1 \
      -combi-modes "drun,files" \
      -modes "combi,drun,files:$HOME/.config/rofi/file.sh" \
      -disable-history \
      -theme $HOME/.config/rofi/config.rasi \
      -no-sort \
-     -show-icons false \
+     -show-icons true \
+     -click-to-exit true &
